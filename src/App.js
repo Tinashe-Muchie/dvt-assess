5930c982c6d03a1b271e7aace49803650fe43bba
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-04-18 22:52:42 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import GlobalContext from './Context/GlobalContext';
+import Details from './components/Details/Details';
+import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Helmet } from 'react-helmet';
+
+class App extends Component {
+  render() {
+    return (
+      <GlobalContext>
+        <Helmet>
+          <style>{'body {background-color: #0B0C10}'}</style>
+        </Helmet>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/details">
+              <Details />
+            </Route>
+          </Switch>
+        </Router>
+      </GlobalContext>
+    );
+  }
+}
+
+export default App;
